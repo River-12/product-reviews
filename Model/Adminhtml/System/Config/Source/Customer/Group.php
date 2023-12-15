640b434ab2da5dd018ad55c0d4c757db9c5ab849
River-12 <147495025+River-12@users.noreply.github.com>
2023-12-15 22:13:32 +0530

    Update Group.php

--- Model/Adminhtml/System/Config/Source/Customer/Group.php
+++ Model/Adminhtml/System/Config/Source/Customer/Group.php
@@ -2,7 +2,7 @@
 
 namespace Riverstone\ProductReviews\Model\Adminhtml\System\Config\Source\Customer;
 
-use Magento\Customer\Model\ResourceModel\Group\CollectionFactory;
+use Magento\Customer\Model\ResourceModel\Group\CollectionFactory as customerFactory;
 use Magento\Framework\Option\ArrayInterface;
 
 class Group implements ArrayInterface
@@ -11,13 +11,15 @@ class Group implements ArrayInterface
      * phpcs:disable
      */
     protected $_options;
+    
+    protected $groupCollectionFactory;
 
     /**
      * @param CollectionFactory $groupCollectionFactory
      */
-    public function __construct(CollectionFactory $groupCollectionFactory)
+    public function __construct(customerFactory $groupCollectionFactory)
     {
-        $this->_groupCollectionFactory = $groupCollectionFactory;
+        $this->groupCollectionFactory = $groupCollectionFactory;
     }
 
     /**
@@ -28,7 +30,7 @@ class Group implements ArrayInterface
     public function toOptionArray()
     {
         if (!$this->_options) {
-            $this->_options = $this->_groupCollectionFactory->create()->loadData()->toOptionArray();
+            $this->_options = $this->groupCollectionFactory->create()->loadData()->toOptionArray();
         }
         return $this->_options;
     }
